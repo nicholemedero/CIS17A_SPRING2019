108e5fb1f907a7f0a004607ecc25f169cc3ae447
Nichole Medero <nmedero@student.rccd.edu>
2019-05-06 12:11:00 -0700

    .

--- Assign_6/genericAccount.cpp
+++ Assign_6/genericAccount.cpp
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   genericAccount.cpp
- * Author: nmedero
- * 
- * Created on May 6, 2019, 11:37 AM
- */
-
 #include "genericAccount.h"
 
 genericAccount::genericAccount() {
--- Assign_6/genericAccount.h
+++ Assign_6/genericAccount.h
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   genericAccount.h
- * Author: nmedero
- *
- * Created on May 6, 2019, 11:36 AM
- */
 
 #ifndef GENERICACCOUNT_H
 #define GENERICACCOUNT_H
